49cbb674ecb5c6476d5c76a0e5ba3e0f179b147b
nico_floriani <floriani.nico@gmail.com>
2023-04-29 17:27:45 -0700

    Created keydown event listener and refactored using functions

--- js/script.js
+++ js/script.js
@@ -2,12 +2,25 @@ var button = document.querySelector(".got-this");
 var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
-button.addEventListener("click", function () {
-    modal.classList.add("show-modal");
-    button.innerText = "You've got this!!!";
-});
-
-modalX.addEventListener("click", function () {
+var closeModal = function () {
     modal.classList.remove("show-modal");
     button.innerText = "Who's got this?";
-});
+  };
+  
+  button.addEventListener("click", function () {
+    modal.classList.add("show-modal");
+    button.innerText = "You've got this!!!";
+  });
+  
+  modalX.addEventListener("click", function () {
+    closeModal();
+  });
+  
+  document.addEventListener("keydown", function (e) {
+    // console.log(e.key);
+    if (e.key === "Escape") {
+      if (modal.classList.contains("show-modal")) {
+        closeModal();
+      }
+    }
+  });
